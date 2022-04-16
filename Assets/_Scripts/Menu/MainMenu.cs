661c17c1704180f27fc1cf4dc0373d4b974b3a1c
Vineet Kumar <megazeroxzvk@gmail.com>
2022-04-15 20:43:53 -0400

    Main Menu Scene and UI completed

--- Assets/_Scripts/Menu/MainMenu.cs
+++ Assets/_Scripts/Menu/MainMenu.cs
@@ -1,18 +1,96 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Scenes
+/// </summary>
+public enum Scenes
+{
+    START_SCENE,
+    GAME_SCENE,
+    END_SCENE,
+    TOTAL_SCENES
+}
+
+/// <summary>
+/// Buttons
+/// </summary>
+public enum Buttons
+{
+    START_BUTTON,
+    INSTRUCTIONS_BUTTON,
+    CREDITS_BUTTON,
+    BACK_BUTTON,
+    EXIT_BUTTON,
+    RESUME_BUTTON,
+    MENU_BUTTON,
+    TOTAL_BUTTONS
+}
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject MenuPanel;
+    public GameObject InstructionsPanel;
+    public GameObject CreditsPanel;
+
+
+    private void Start()
     {
-        
+        MenuPanel.SetActive(true);
+        InstructionsPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Start game button
+    /// </summary>
+    public void Button_OnStartGamePressed()
     {
-        
+        SceneManager.LoadScene(1);
+    }
+
+    /// <summary>
+    /// Start game button
+    /// </summary>
+    public void Button_OnInstructionsPressed()
+    {
+        MenuPanel.SetActive(false);
+        InstructionsPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+    }
+
+    /// <summary>
+    /// Start game button
+    /// </summary>
+    public void Button_OnCreditsPressed()
+    {
+        MenuPanel.SetActive(false);
+        InstructionsPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+    }
+
+    /// <summary>
+    /// Start game button
+    /// </summary>
+    public void Button_OnExitPressed()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
+    }
+
+    /// <summary>
+    /// Back button
+    /// </summary>
+    public void Button_OnBackButtonPressed()
+    {
+        MenuPanel.SetActive(true);
+        InstructionsPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
     }
 }
